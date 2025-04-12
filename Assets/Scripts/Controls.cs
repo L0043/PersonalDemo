8cb7f2a49ae47cf826d61b792002d6dae69f0a20
L0043 <lill0043@algonquinlive.com>
2025-04-12 04:27:55 -0400

    added basic movement using input system

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(PlayerInput))]
+public class Controls : MonoBehaviour
+{
+    [SerializeField] Rigidbody _rigidbody;
+    public float MoveSpeed = 5f;
+    Vector2 _movementDirection = new Vector2();
+    InputAction _aimAction;
+    InputAction _forwardAction;
+    InputAction _backwardAction;
+    InputAction _leftAction;
+    InputAction _rightAction;
+    InputAction _jumpAction;
+    InputAction _dashAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!_rigidbody)
+            _rigidbody = GetComponent<Rigidbody>();
+        
+        var actionMap = GetComponent<PlayerInput>().currentActionMap;
+
+        _aimAction = actionMap["Aim"];
+        _forwardAction = actionMap["Forward"];
+        _backwardAction = actionMap["Backward"];
+        _leftAction = actionMap["Left"];
+        _rightAction = actionMap["Right"];
+        _jumpAction = actionMap["Jump"];
+        _dashAction = actionMap["Dash"];
+
+    }
+
+
+    private void OnEnable()
+    {
+        _aimAction.started += ctx => AimActionRecieved();
+        _forwardAction.started += ctx => ForwardActionRecieved();
+        _backwardAction.started += ctx => BackwardActionRecieved();
+        _leftAction.started += ctx => LeftActionRecieved();
+        _rightAction.started += ctx => RightActionRecieved();
+        _jumpAction.started += ctx => JumpActionRecieved();
+        _dashAction.started += ctx => DashActionRecieved();
+    }
+
+    void AimActionRecieved() 
+    {
+        
+    }
+
+    void ForwardActionRecieved() 
+    {
+        _movementDirection.x = 1;
+
+    }
+
+    void BackwardActionRecieved() 
+    {
+        _movementDirection.x = -1;
+    }
+
+    void LeftActionRecieved() 
+    {
+        _movementDirection.y = 1;
+        
+    }
+    void RightActionRecieved() 
+    {
+        _movementDirection.y = -1;   
+    }
+    void JumpActionRecieved() 
+    {
+
+        
+    }
+    void DashActionRecieved() 
+    {
+        
+    }
+    private void OnDisable()
+    {
+        _aimAction.started -= ctx => AimActionRecieved();
+        _forwardAction.started -= ctx => ForwardActionRecieved();
+        _backwardAction.started -= ctx => BackwardActionRecieved();
+        _leftAction.started -= ctx => LeftActionRecieved();
+        _rightAction.started -= ctx => RightActionRecieved();
+        _jumpAction.started -= ctx => JumpActionRecieved();
+        _dashAction.started -= ctx => DashActionRecieved();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_movementDirection != Vector2.zero)
+        {
+            Vector3 movement = new Vector3(_movementDirection.x, 0, _movementDirection.y);
+            _rigidbody.AddForce(movement * MoveSpeed, ForceMode.Impulse);
+        }
+        
+    }
+}
